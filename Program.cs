eaf310b08330e2b672d05f7ac58a03c6562327c2
Eduarda Ferreira <edududaferreira10@gmail.com>
2022-09-30 21:20:05 -0300

    30/09

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int n1, n2;
+
+Console.WriteLine("Divisão de dois números");
+
+Console.Write("Digite o primeiro número: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n2 == 0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+
+    Console.WriteLine("Não é possível dividir por zero.");
+
+    Console.ResetColor();
+}
+else 
+{
+    double resultado = n1 / n2; 
+    Console.Write($"{n1} dividido por {n2} é ");
+    
+    Console.ForegroundColor = ConsoleColor.Green;
+    
+     Console.WriteLine($"{resultado}");
+
+    Console.ResetColor();
+
+}
